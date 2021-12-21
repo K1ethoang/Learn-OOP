82075702f328dcf5b6d8a61bdd174760cda48ad9
Hoang Gia Kiet <kiethoang101@gmail.com>
2021-12-21 09:24:37 +0700

    cơ bản template

---
+++ Template_Study/Function_Template/code.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ! Function Template
+// cùng kiểu dữ liệu
+template <class T>
+void swap_(T &a, T &b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+// 2 kiểu dữ liệu khác nhau
+template <class T, class X>
+void _swap(T &a, X &b)
+{
+    T temp = a;
+    a = (T)b;
+    b = (X)temp;
+}
+
+// ! Class Template
+template <class Type>
+class Point
+{
+private:
+    Type x;
+    Type y;
+
+public:
+    Point()
+    {
+        this->x = 0;
+        this->y = 0;
+    }
+
+    Point(const Type &x_, const Type &y_)
+        : x(x_), y(y_) {}
+
+    friend istream &operator>>(istream &is, Point &p)
+    {
+        cout << "\nNhap toa do x: ";
+        is >> p.x;
+        cout << "\nNhap toa do y: ";
+        is >> p.y;
+        return is;
+    }
+
+    friend ostream &operator<<(ostream &os, Point p)
+    {
+        os << "(" << p.x << ", " << p.y << ")";
+        return os;
+    }
+};
+
+int main()
+{
+    Point<double> point;
+    cin >> point;
+    cout << point;
+    return 0;
+}
